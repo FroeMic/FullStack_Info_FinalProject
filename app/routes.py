cbeef4e8d2f76fa95061951ce6b0426986ee1c7b
Michael Froehlich <m.froehlich@tum.de>
2018-04-04 16:39:50 -0700

    Add register route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,33 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user
 
+from app import app, db
+from app.forms import RegistrationForm
+from app.models import User
+
+
+# =========================
+# Anonymously Accessible 
+# =========================
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Hello World')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, firstname=form.firstname.data, lastname=form.lastname.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(user.firstname + ', we successfully registered your account!', 'info')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return "TODO: Login route not implemented yet"
