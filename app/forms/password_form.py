39177838d2d0ec1c3242ce19f45e0a6ffcbea99d
daphnejong <daphne.jong@berkeley.edu>
2018-04-09 10:58:22 -0700

    made changes to change password - enter new password twice, notification that password has updated

--- app/forms/password_form.py
+++ app/forms/password_form.py
@@ -7,6 +7,6 @@ from app.models import User
 
 class PasswordForm(Form):
     currentpassword = PasswordField('Current Password', render_kw={'class': 'form-control', 'placeholder': 'enter current password', 'required': True}, validators=[DataRequired('Please enter your current password.')])
-    confirmpassword = PasswordField('Re-Enter Current Password', render_kw={'class': 'form-control', 'placeholder': 're-enter your password', 'required': True}, validators=[DataRequired(), EqualTo('currentpassword')])
-    newpassword = PasswordField('Password', render_kw={'class': 'form-control', 'placeholder': '6+ characters', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired('Please enter a new password.')])
+    newpassword = PasswordField('New Password', render_kw={'class': 'form-control', 'placeholder': '6+ characters', 'required': True, 'pattern': '.{6,}', 'title': 'Minimum 6 characters'}, validators=[DataRequired('Please enter a new password.')])
+    confirmpassword = PasswordField('Re-Enter New Password', render_kw={'class': 'form-control', 'placeholder': 're-enter your password', 'required': True}, validators=[DataRequired(), EqualTo('newpassword')])
     submit = SubmitField('Update Password', render_kw={'class': 'btn btn-primary btn-round'} )
--- app/routes.py
+++ app/routes.py
@@ -97,8 +97,8 @@ def settings_password():
         if not user.check_password(form.currentpassword.data):
             flash('Error! Your current password appears to be incorrect.', 'error')
             return redirect(url_for('settings_password'))
-
         user.set_password(form.newpassword.data)
         db.session.commit()
+        flash('Your password was updated!', 'info')
         return redirect(url_for('settings_password'))
     return render_template('settings_password.html', title='Settings', form=form)
