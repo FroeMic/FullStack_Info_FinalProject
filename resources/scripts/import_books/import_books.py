c433dd5b63a71fd10387503b3d4458ac86b8e8a9
Michael Froehlich <m.froehlich@tum.de>
2018-04-12 15:49:38 -0700

    Load input file into pandas frame and analyze it

--- resources/scripts/import_books/import_books.py
+++ resources/scripts/import_books/import_books.py
@@ -2,12 +2,14 @@ import sys
 import os
 import getopt
 import termcolor
+import pandas as pd
 
 
 # CONFIG
 # =============
 DB_PATH = None
 INPUT_FILE =  None
+INPUT_FILE_SEPERATOR = ';'
 
 DB_BOOK_TABLE = 'book'
 DB_MOOD_TABLE = 'mood'
@@ -19,7 +21,7 @@ DB_MOOD_COLUMN = 'title'
 
 INPUT_ISBN_COLUMN = 'isbn'
 INPUT_ISBN13_COLUMN = 'isbn13'
-INPUT_IGNORE_COLUMNS = [ 'title' ]
+INPUT_IGNORE_COLUMNS = [ 'book_title' ]
 
 # ===========
 # CLI helpers
@@ -30,7 +32,7 @@ def parse_arguments():
     global INPUT_FILE
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hd:i:",["help"])
+        opts, args = getopt.getopt(sys.argv[1:],"hd:i:s:",["help", "seperator"])
     except getopt.GetoptError:
         print_usage_and_exit()
 
@@ -41,6 +43,8 @@ def parse_arguments():
              DB_PATH = arg
         elif opt in ('-i'):
              INPUT_FILE = arg
+        elif opt in ('-s', '--seperator'):
+             INPUT_FILE_SEPERATOR = arg
 
     if not all([DB_PATH, INPUT_FILE]):
         print_usage_and_exit()
@@ -53,6 +57,11 @@ def print_usage_and_exit():
 def log_error(str):
     ''' Prints a string in red color '''
     print(termcolor.colored(str, 'red'))
+
+def log_warning(str):
+    ''' Prints a string in yellow color '''
+    print(termcolor.colored(str, 'yellow'))
+
 
 def log_info(str):
     ''' Prints a string in blue color '''
@@ -70,6 +79,36 @@ def input_file_exists():
     ''' Returns whether the INPUT_FILE points to a valid file '''
     return os.path.exists(INPUT_FILE) and os.path.isfile(INPUT_FILE)
 
+def load_input_file():
+    '''Loads the input file into a pandas dataframe'''
+    return pd.read_csv(INPUT_FILE, sep=INPUT_FILE_SEPERATOR)
+
+def drop_ignored_columns(df):
+    '''Removes the columns specified in INPUT_IGNORE_COLUMNS from the dataframe'''
+    columns_to_drop = [col for col in INPUT_IGNORE_COLUMNS if col in df.columns]
+    return df.drop(columns_to_drop, axis=1)
+
+def check_whether_required_columns_exist(df):
+    '''Check whether all required columns exist'''
+    if not INPUT_ISBN_COLUMN:
+        log_error('Required column "' + INPUT_ISBN_COLUMN + '" is missing in the input file.')
+    if not INPUT_ISBN13_COLUMN:
+        log_error('Required column "' + INPUT_ISBN13_COLUMN + '" is missing in the input file.')
+
+def drop_duplicate_rows(df):
+    ''' Removes duplicate rows based on INPUT_ISBN_COLUMN and INPUT_ISBN13_COLUMN columns '''
+    df_unique = df.drop_duplicates(subset = [INPUT_ISBN_COLUMN])
+    df_unique = df.drop_duplicates(subset = [INPUT_ISBN13_COLUMN])
+    number_of_removed_columns = len(df) - len(df_unique)
+    if (number_of_removed_columns > 0):
+        log_warning('Removed ' + str(number_of_removed_columns) + ' duplicate rows.')
+    return df_unique
+
+def get_mood_labels(df):
+    ''' Returns a dictionary of all moods that should be inserted into the db. ''' 
+    not_mood_columns = [INPUT_ISBN13_COLUMN, INPUT_ISBN_COLUMN] + INPUT_IGNORE_COLUMNS
+    return [{'mood_title': col, 'id': None} for col in df.columns if col not in not_mood_columns]
+
 # ===========
 # Main routines
 # ===========
@@ -85,12 +124,29 @@ def run():
         log_error('Error: Could not locate the input file at "' + INPUT_FILE +'"')
         exit(2)
 
-    # 3. Check whether required table exist (books, mood, book_mood)
-    # 4. Check whether all required columns exist (isbn, isbn13, mood1 ... mood n)
+    # 3. Check whether all required columns exist (isbn, isbn13, mood1 ... mood n)
         # 4.1 Print log statement
         # Importing x moods
         # Importing x books
         # Skipping x books due to missing isbn or isbn13 values.
+
+    log_info('Analyzing input file ...')
+    df = load_input_file()
+    df = drop_ignored_columns(df)
+    df = drop_duplicate_rows(df)
+    check_whether_required_columns_exist(df)
+    moods =  get_mood_labels(df)
+    log_info('Input file looks good:')
+    log_info('\t-> Found ' + str(len(moods)) + ' moods to import.')
+    log_info('\t-> Found ' + str(len(df)) + ' books to import.')
+
+    # print(moods)
+    # print(df.head())
+    # print(df.columns)
+
+    log_info('Checking database connection ...')
+
+    # 4. Check whether required tables exist (books, mood, book_mood)
     # 5. Check which moods exist in the database by getting their ids
         # 5.1 Print log statement
         # xx moods exits in db already ... skipping
