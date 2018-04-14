5da64021a5f50d45dc8849847846d9a49b2c542f
Michael Froehlich <m.froehlich@tum.de>
2018-04-13 17:41:42 -0700

    Configure app with SQLite queue

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,18 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
+
+# Configure queue
+from app.utils.flask_sqlite_queue import Job, create_queue, configure_scheduler
+# If flask is run with the `use_reloader` set to True
+# the queue will be spawned to times. 
+# Be sure to disable this flag for production deployment.
+# app.run(debug=True, host=app.config['HOST'], port=app.config['PORT'], use_reloader=True)
+
+configure_scheduler(cycle_interval=1.0)
+queue = create_queue(app.config['JOB_QUEUE_DATABASE_URI'])
+
+
 from app import routes, models, forms, utils
 
 
