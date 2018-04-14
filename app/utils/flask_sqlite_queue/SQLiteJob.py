bd4d9c78d0c74be9ec56ef56fefda2e5abdcd95e
Michael Froehlich <m.froehlich@tum.de>
2018-04-13 17:41:18 -0700

    Add flask_sqlite_queue

---
+++ app/utils/flask_sqlite_queue/SQLiteJob.py
@@ -0,0 +1,7 @@
+class SQLiteJob(object):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
---
+++ app/utils/flask_sqlite_queue/__init__.py
@@ -0,0 +1,3 @@
+from .SQLiteJobHandler import create_queue, remove_queue, configure_scheduler, stop_all
+from .NotAJobTypeWarning import NotAJobTypeWarning
+from .SQLiteJob import SQLiteJob as Job
