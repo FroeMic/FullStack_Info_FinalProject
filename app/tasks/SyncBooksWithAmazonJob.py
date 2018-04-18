69d30b6fb2c3c82b6737d6946e6c171483030407
daphnejong <daphne.jong@berkeley.edu>
2018-04-17 21:59:20 -0700

    Populated the database with amazon URL and price

--- app/tasks/SyncBooksWithAmazonJob.py
+++ app/tasks/SyncBooksWithAmazonJob.py
@@ -1,11 +1,36 @@
+from app.utils.flask_sqlite_queue import Job
 from app import app, queue, db
+from app.models import Book, Genre
+
+from datetime import datetime, timedelta
+
 from amazon.api import AmazonAPI, AsinNotFound
 
-amazon = AmazonAPI(app.config['AMAZON_ACCESS_KEY'], app.config['AMAZON_SECRET_KEY'], app.config['AMAZON_ASSOC_TAG'])
+class SyncBooksWithAmazonJob(Job):
+    
+    def __init__(self, repeat = True):
+        Job.__init__(self)
+        self.repeat = repeat
+    
+    def run(self):
+        if self.repeat:
+            self._schedule_next_run()
+        for book in Book.query.all():
+            self._sync_book_with_amazon(book)
 
-try:
-	products = amazon.lookup(SearchIndex='All', IdType='ISBN', ItemId='9780450040184')
+    def _schedule_next_run(self):
+        today = datetime.today()
+        tomorrow = today + timedelta(days=1)
+        tomorrow_1_am = datetime(tomorrow.year, tomorrow.month, tomorrow.day, hour = 1, tzinfo = None )
 
-	print(products[0].offer_url)
-except AsinNotFound:
-	print('No Result')
+        queue.schedule(self, tomorrow_1_am)
+
+    def _sync_book_with_amazon(self, book):
+        amazon = AmazonAPI(app.config['AMAZON_ACCESS_KEY'], app.config['AMAZON_SECRET_KEY'], app.config['AMAZON_ASSOC_TAG'])
+
+        products = amazon.lookup(SearchIndex='Books', IdType='ISBN', ItemId=book.isbn13)
+
+        book.amazon_url = products[0].offer_url
+        book.price = products[0].price_and_currency[0]
+
+        db.session.commit()
